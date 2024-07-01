88fdba4e832643e9cec05c16524ac13f81bf713f
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-07-01 17:32:19 +0500

    1

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,85 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useStore } from "./store";
 import Header from "./pages/Header";
 import Footer from "./pages/Footer";
+import { useState } from "react";
 function App() {
   const queryClient = useQueryClient();
+  const [select, setSelect] = useState("all");
+  const [search, setSearch] = useState("");
+
   const { data, isLoading, error } = useQuery({
-    queryKey: ["select"],
+    queryKey: [select, search],
     queryFn: async () => {
-      const req = await fetch("https://dummyjson.com/products");
+      let url = "https://dummyjson.com/products";
+      if (select !== "all") {
+        url = `https://dummyjson.com/products/category/${select}`;
+      }
+      if (search) {
+        url = `https://dummyjson.com/products/search?q=${search}`;
+      }
+      const req = await fetch(url);
       const res = await req.json();
       return res?.products;
     },
   });
+
+  const { data: categories } = useQuery({
+    queryKey: ["categories"],
+    queryFn: async () => {
+      const req = await fetch("https://dummyjson.com/products/categories");
+      const res = await req.json();
+      return res;
+    },
+  });
+
   return (
     <>
       <Header />
       <main className="container mt-5 mb-5">
         <section>
+          <div className="flex gap-24">
+            <div className="flex">
+              <button className="btn btn-ghost btn-circle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-5.5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
+              <input
+                type="text"
+                placeholder="Type here"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+                className="input input-bordered input-accent w-full max-w-xs"
+              />
+            </div>
+            <div className="flex">
+              {categories && (
+                <select
+                  value={select}
+                  onChange={(e) => setSelect(e.target.value)}
+                  className="select select-info w-full max-w-xs"
+                >
+                  <option value="all">Filter by category</option>
+                  {categories.map(({ slug, name, url }) => (
+                    <option key={slug} value={slug}>
+                      {name}
+                    </option>
+                  ))}
+                </select>
+              )}
+            </div>
+          </div>
           <div className="grid gap-14 grid-cols-1 md:grid-cols-2 xl:grid-cols-3  md:gap-24 xl:gap-x-48 mt-10 pb-20 strech">
             {data &&
               data.map(
@@ -29,7 +93,7 @@ function App() {
                       <div className="card w-[320px] bg-base-100 shadow-[0_3px_15px_0] shadow-primary">
                         <figure>
                           <img
-                            className="w-[320px] h-[320px] object-cover object-center"
+                            className="w-[320px] h-[320px] object-center object-scale-down"
                             src={
                               images ? (
                                 images[0]
--- src/pages/Header/index.jsx
+++ src/pages/Header/index.jsx
@@ -48,22 +48,6 @@ function Header() {
               <a className="btn btn-ghost text-xl">AbuFa Market</a>
             </div>
             <div className="navbar-end">
-              <button className="btn btn-ghost btn-circle">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-5.5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                  />
-                </svg>
-              </button>
               <div className="dropdown dropdown-end">
                 <div
                   tabIndex={0}
